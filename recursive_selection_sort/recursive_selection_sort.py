174304f1f6764d1f3bf6b646901ac22c450866a3
jsteinberg-rbi <jsteinberg@rbi.com>
2024-02-27 12:34:29 -0600

    multiple refinements made -- ship it.

--- recursive_selection_sort/recursive_selection_sort.py
+++ recursive_selection_sort/recursive_selection_sort.py
@@ -1,16 +1,16 @@
-def recursive_selection_sort(arr: list, i=0, j=1, min_index = 0):
+def recursive_selection_sort(arr: list, i: int=0):
     
     # base case
     if i > len(arr)-2:
         return arr
     
     # iteration
-    for j in range(j, len(arr)):
-        if arr[j] < arr[min_index]:
-            min_index = j
-            arr[i], arr[min_index] = arr[min_index], arr[i]
-            min_index = i
+    min_index = i
+    for idx in range(i+1, len(arr)):
+        if arr[idx] < arr[min_index]:
+            min_index = idx
+    arr[i], arr[min_index] = arr[min_index], arr[i]
     # recursion
-    recursive_selection_sort(arr, i+1, i+2, min_index=i+1)
+    recursive_selection_sort(arr, i+1)
     # uncertain if this is needed
     return arr
--- recursive_selection_sort/test_recursive_selection_sort.py
+++ recursive_selection_sort/test_recursive_selection_sort.py
@@ -3,56 +3,56 @@ import pytest
 
 def test_alternating_zeroes_and_ones():
     arr = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
-    assert recursive_selection_sort(arr, i=0, j=1, min_index = 0) == sorted(arr)
+    assert recursive_selection_sort(arr, i=0) == sorted(arr)
 
 def test_increasing_decreasing():
     arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-    assert recursive_selection_sort(arr, i=0, j=1, min_index = 0) == sorted(arr)
+    assert recursive_selection_sort(arr, i=0) == sorted(arr)
 
 def test_decreasing_increasing():
     arr = [19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1] + list(range(1, 20))
-    assert recursive_selection_sort(arr, i=0, j=1, min_index = 0) == sorted(arr)
+    assert recursive_selection_sort(arr, i=0) == sorted(arr)
 
 def test_all_but_one_element_the_same():
     arr = [0] * 19 + [1]
-    assert recursive_selection_sort(arr, i=0, j=1, min_index = 0) == sorted(arr)
+    assert recursive_selection_sort(arr, i=0) == sorted(arr)
 
 def test_all_but_one_element_the_same_big_delta():
     arr = [1] * 19 + [100]
-    assert recursive_selection_sort(arr, i=0, j=1, min_index = 0) == sorted(arr)
+    assert recursive_selection_sort(arr, i=0) == sorted(arr)
 
 def test_large_range():
     arr = [-100] + list(range(1, 20))
-    assert recursive_selection_sort(arr, i=0, j=1, min_index = 0) == sorted(arr)
+    assert recursive_selection_sort(arr, i=0) == sorted(arr)
 
 def test_very_close_numbers():
     arr = [i/10 for i in range(20)]
-    assert recursive_selection_sort(arr, i=0, j=1, min_index = 0) == sorted(arr)
+    assert recursive_selection_sort(arr, i=0) == sorted(arr)
 
 def test_all_negative_numbers():
     arr = list(range(0, -5, -1))
-    assert recursive_selection_sort(arr, i=0, j=1, min_index = 0) == sorted(arr)
+    assert recursive_selection_sort(arr, i=0) == sorted(arr)
 
 def test_mix_of_positive_and_negative():
     arr = [-10, 10] * 10
-    assert recursive_selection_sort(arr, i=0, j=1, min_index = 0) == sorted(arr)
+    assert recursive_selection_sort(arr, i=0) == sorted(arr)
 
 def test_alternating_high_and_low():
     arr = [20 - i if i % 2 == 0 else i for i in range(20)]
-    assert recursive_selection_sort(arr, i=0, j=1, min_index = 0) == sorted(arr)
+    assert recursive_selection_sort(arr, i=0) == sorted(arr)
 
 def test_peak_in_the_middle():
     arr = [2] * 10 + [3] + [2] * 9
-    assert recursive_selection_sort(arr, i=0, j=1, min_index = 0) == sorted(arr)
+    assert recursive_selection_sort(arr, i=0) == sorted(arr)
 
 def test_small_range_random_integers():
     arr = [5, 3, 0, 4, 0, 4, 4, 0, 4, 0, 0, 0, 0, 4, 1, 0, 1, 4, 3, 5]
-    assert recursive_selection_sort(arr, i=0, j=1, min_index = 0) == sorted(arr)
+    assert recursive_selection_sort(arr, i=0) == sorted(arr)
 
 def test_floating_point_numbers():
     arr = [float(i) for i in range(20)]
-    assert recursive_selection_sort(arr, i=0, j=1, min_index = 0) == sorted(arr)
+    assert recursive_selection_sort(arr, i=0) == sorted(arr)
 
 def test_large_value_at_the_beginning():
     arr = [1000] + list(range(1, 20))
-    assert recursive_selection_sort(arr, i=0, j=1, min_index = 0) == sorted(arr)
+    assert recursive_selection_sort(arr, i=0) == sorted(arr)
