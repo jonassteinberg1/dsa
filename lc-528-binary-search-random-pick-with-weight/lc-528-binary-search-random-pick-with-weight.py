c13f21eed0b709b5f40f2f0a1d1f2c863f9e28d3
jsteinberg-rbi <jsteinberg@rbi.com>
2025-09-09 18:57:47 -0400

    working binary search list population

--- lc-528-binary-search-random-pick-with-weight/lc-528-binary-search-random-pick-with-weight.py
+++ lc-528-binary-search-random-pick-with-weight/lc-528-binary-search-random-pick-with-weight.py
@@ -8,16 +8,15 @@ class Solution:
         self.w = w
         s = sum(self.w)
         self.prefix_sums = []
-        self.prefix_sums.append((1, self.w[0]))
-        self.prefix_sums.append((self.w[0] + 1, self.w[0] + self.w[1]))
-        for idx, val in enumerate(self.w[2:]):
-        #for idx, val in enumerate(self.w):
-            if idx < len(self.w) - 1:
-                self.prefix_sums.append((val + 1, (val + 1) + self.w[idx+1]))
+        self.prefix_sums.append((1, self.w[0])) # need to handle for 1 in w
+        self.prefix_sums.append((self.w[0] + 1, self.w[0] + self.w[1])) # need to handle for 1 in w
+        for idx, val in enumerate(self.w, start=2):
+            #if idx < len(self.w):
+            self.prefix_sums.append((self.prefix_sums[idx-1][1] + 1, sum(self.w[0:idx+1])))
+
 
     def pickIndex(self) -> int:
-        #return choice(self.probs)
-        return self.prefix_sums
+        return choice(self.w)
 
 s = Solution([1, 2, 3, 4 ,5])
 print(s.pickIndex())
