f3bc024eb277d6cd0d6df638788cb966195a8ede
jsteinberg-rbi <jsteinberg@rbi.com>
2025-09-09 09:32:20 -0400

    closing in on core prefixSum algo

--- lc-528-binary-search-random-pick-with-weight/lc-528-binary-search-random-pick-with-weight.py
+++ lc-528-binary-search-random-pick-with-weight/lc-528-binary-search-random-pick-with-weight.py
@@ -6,15 +6,18 @@ class Solution:
 
     def __init__(self, w: List[int]):
         self.w = w
-        self.probs = []
-        for idx, el in enumerate(self.w):
-            for r in range(0, el):
-                self.probs.append(idx)
+        s = sum(self.w)
+        self.prefix_sums = []
+        self.prefix_sums.append((1, self.w[0]))
+        self.prefix_sums.append((self.w[0] + 1, self.w[0] + self.w[1]))
+        for idx, val in enumerate(self.w[2:]):
+        #for idx, val in enumerate(self.w):
+            if idx < len(self.w) - 1:
+                self.prefix_sums.append((val + 1, (val + 1) + self.w[idx+1]))
 
     def pickIndex(self) -> int:
-        return choice(self.probs)
+        #return choice(self.probs)
+        return self.prefix_sums
 
 s = Solution([1, 2, 3, 4 ,5])
 print(s.pickIndex())
-
-
