80b7d151bf9825ba6a3d60c2bcc78fb12436d31a
jsteinberg-rbi <jsteinberg@rbi.com>
2025-09-12 06:30:33 -0400

    non-overlapping cases

--- lc-4-median-of-two-sorted-arrays/lc-4-median-of-two-sorted-arrays.py
+++ lc-4-median-of-two-sorted-arrays/lc-4-median-of-two-sorted-arrays.py
@@ -6,9 +6,17 @@ class Solution:
         self.nums1 = nums1
         self.nums2 = nums2
 
-        if self.nums1[0] > self.nums2[-1]:
+        if self.nums1[0] >= self.nums2[-1]:
+            if len(self.nums1 + self.nums2) % 2 == 0:
+                middle = len(self.nums2 + self.nums1) // 2
+                return sum((self.nums2 + self.nums1)[middle-1:middle+1]) / 2
+            else:
+                return (self.nums2 + self.nums1)[len(self.nums2 + self.nums1) // 2]
+        elif self.nums1[-1] <= self.nums2[0]:
             if len(self.nums1 + self.nums2) % 2 == 0:
                 middle = len(self.nums1 + self.nums2) // 2
-                return sum((self.nums1 + self.nums2)[middle:middle+1]) / 2
+                return sum((self.nums1 + self.nums2)[middle-1:middle+1]) / 2
+            else:
+                return (self.nums1 + self.nums2)[len(self.nums1 + self.nums2) // 2]
 
-print(Solution.findMedianSortedArrays([1,2,3], [0]))
+print(Solution.findMedianSortedArrays([1, 2, 3], [4, 5, 6]))
