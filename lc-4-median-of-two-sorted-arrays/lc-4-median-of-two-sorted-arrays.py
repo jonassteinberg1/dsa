560acde9528aad8da5f065e4a9f1d950d4f3e6a0
jsteinberg-rbi <jsteinberg@rbi.com>
2025-09-13 17:03:51 -0400

    getting very close (I think)

--- lc-4-median-of-two-sorted-arrays/lc-4-median-of-two-sorted-arrays.py
+++ lc-4-median-of-two-sorted-arrays/lc-4-median-of-two-sorted-arrays.py
@@ -18,6 +18,38 @@ class Solution:
                 return sum((self.nums1 + self.nums2)[middle-1:middle+1]) / 2
             else:
                 return (self.nums1 + self.nums2)[len(self.nums1 + self.nums2) // 2]
+        elif len(self.nums1) < 2:
+            lo = 0
+            hi = self.nums2[-1]
+            while lo <= hi:
+                middle = lo + (high - lo) // 2
+                if self.nums1[0] == middle:
+                    if len(self.nums1 + self.nums2) % 2 == 0:
+                        middle = len(self.nums1 + self.nums2) // 2
+                        return sum(sorted(self.nums1 + self.nums2)[middle-1:middle+1]) / 2
+                    else:
+                        return (sorted(self.nums1 + self.nums2))[len(self.nums1 + self.nums2) // 2]
+                elif self.nums1 > middle:
+                    lo = middle + 1
+                else:
+                    high = middle - 1
+        elif len(self.nums2) < 2:
+            lo = 0
+            hi = self.nums1[-1]
+            while lo <= hi:
+                middle = lo + (high - lo) // 2
+                if self.nums2[0] == middle:
+                    if len(self.nums1 + self.nums2) % 2 == 0:
+                        middle = len(self.nums1 + self.nums2) // 2
+                        return sum(sorted(self.nums1 + self.nums2)[middle-1:middle+1]) / 2
+                    else:
+                        return (sorted(self.nums1 + self.nums2))[len(self.nums1 + self.nums2) // 2]
+                elif self.nums2 > middle:
+                    lo = middle + 1
+                else:
+                    high = middle - 1
+    
+            
         else:
             if len(self.nums1) <= len(self.nums2):
                 short = self.nums1
@@ -25,21 +57,21 @@ class Solution:
             else:
                 short = self.nums2
                 long = self.nums1
-            half = (len(short) + len(long) + 1) / 2
-            lo = max(0, half - long)
-            hi = min(short, half)
-            i = lo + (hi - lo) // 2
-            j = half - i
+            half = (len(short) + len(long) + 1) // 2
+            lo = max(0, half - len(long))
+            hi = min(len(short), half)
             while lo <= hi:
+                i = lo + (hi - lo) // 2
+                j = half - i
                 if short[i-1] <= long[j] and long[j-1] <= short[i]:
                     if len(short + long) % 2 != 0:
                         return max(short[i-1], long[j-1])
                     else:
-                        return min(short[i], long[j])
+                        return (max(short[i-1], long[j-1]) + min(short[i], long[j]))/2
                 elif short[i-1] > long[j]:
-                    lo = i + 1
+                    hi = i - 1
                 else:
-                    high = i - 1
+                    lo = i + 1
 
             
         
@@ -47,4 +79,4 @@ class Solution:
            
 
 
-print(Solution.findMedianSortedArrays([1, 2, 3], [4, 5, 6]))
+print(Solution.findMedianSortedArrays([1, 3, 5, 900], [2]))
