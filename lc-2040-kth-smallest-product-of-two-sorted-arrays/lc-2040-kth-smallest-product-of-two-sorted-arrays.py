15f6c775a18c029b9be64a45ec6efae64061b283
jsteinberg-rbi <jsteinberg@rbi.com>
2025-10-15 18:35:04 -0400

    deep into 3rd or 4th solution; test cases getting pretty far

--- lc-2040-kth-smallest-product-of-two-sorted-arrays/lc-2040-kth-smallest-product-of-two-sorted-arrays.py
+++ lc-2040-kth-smallest-product-of-two-sorted-arrays/lc-2040-kth-smallest-product-of-two-sorted-arrays.py
@@ -2,129 +2,68 @@ from typing import List
 
 class Solution:
     def kthSmallestProduct(self, nums1: List[int], nums2: List[int], k: int) -> int:
-        # l & r neg
-        if nums1[0] < 0 and nums1[-1] < 0 and nums2[0] < 0 and nums2[-1] < 0:
-            min_product = nums1[0] * nums2[0]
-            max_product = nums1[-1] * nums2[-1]
-            cursor1_start = -1
-            cursor1_incrementor = -1
-            cursor2_start = -1
-            cursor2_incrementor = -1
-        # l & r both pos
-        elif nums1[0] >= 0 and nums1[-1] >= 0 and nums2[0] >= 0 and nums2[-1] >= 0:
-            min_product = nums1[0] * nums2[0]
-            max_product = nums1[-1] * nums2[-1]
-            cursor1_start = 0
-            cursor1_incrementor = 1
-            cursor2_start = 0
-            cursor2_incrementor = 1
-        # l neg, r pos
-        elif nums1[0] < 0 and nums1[-1] < 0 and nums2[0] >= 0 and nums2[-1] >= 0:
-            min_product = nums1[0] * nums2[-1]
-            max_product = nums1[-1] * nums2[0]
-            cursor1_start = 0
-            cursor1_incrementor = 1
-            cursor2_start = -1
-            cursor2_incrementor = -1
-        # l pos, r neg
-        elif nums1[0] >= 0 and nums1[-1] >= 0 and nums2[0] < 0 and nums2[-1] < 0:
-            min_product = nums1[-1] * nums2[0]
-            max_product = nums1[0] * nums2[-1]
-            cursor1_start = -1
-            cursor1_incrementor = -1
-            cursor2_start = 0
-            cursor2_incrementor = 1
-        # l pos, r neg-to-pos
-        # TODO
-        elif nums1[0] >= 0 and nums1[-1] >= 0 and nums2[0] < 0 and nums2[-1] >= 0:
-            min_product = nums1[-1] * nums2[0]
-            max_product = nums1[-1] * nums2[-1]
-            cursor1_start = -1
-            cursor1_incrementor = -1
-            cursor2_start = 0
-            cursor2_incrementor = 1
-        # l neg-to-pos, r pos
-        # TODO
-        elif nums1[0] < 0 and nums1[-1] >= 0 and nums2[0] >= 0 and nums2[-1] >= 0:
-            min_product = nums1[0] * nums2[-1]
-            max_product = nums1[-1] * nums2[-1]
-            cursor1_start = 0
-            cursor1_incrementor = 1
-            cursor2_start = -1
-            cursor2_incrementor = -1
-        # l neg, r neg-to-pos
-        elif nums1[0] < 0 and nums1[-1] < 0 and nums2[0] < 0 and nums2[-1] >= 0:
-            min_product = nums1[0] * nums2[-1]
-            max_product = nums1[0] * nums2[0]
-            cursor1_start = -1
-            cursor1_incrementor = -1
-            cursor2_start = 0
-            cursor2_incrementor = 1
-        # l neg-to-pos, r neg
-        elif nums1[0] < 0 and nums1[-1] >= 0 and nums2[0] < 0 and nums2[-1] < 0:
-            min_product = nums1[-1] * nums2[0]
-            max_product = nums1[0] * nums2[0]
-            cursor1_start = -1
-            cursor1_incrementor = -1
-            cursor2_start = 0
-            cursor2_incrementor = 1
-        # l & r neg-to-pos
-        elif nums1[0] < 0 and nums1[-1] >= 0 and nums2[0] < 0 and nums2[-1] >= 0:
-            min_product = nums1[-1] * nums2[0]
-            max_product = nums1[-1] * nums2[-1]
-            cursor1_start = -1
-            cursor1_incrementor = -1
-            cursor2_start = -1
-            cursor2_incrementor = -1
-        low = min_product
-        high = max_product
-        counter = 0
-        ks = []
-        cursor1 = cursor1_start
-        cursor2 = cursor2_start
+        low = min(nums1[0]*nums2[0], nums1[0]*nums2[-1], nums1[-1]*nums2[0], nums1[-1]*nums2[-1])
+        high = max(nums1[0]*nums2[0], nums1[0]*nums2[-1], nums1[-1]*nums2[0], nums1[-1]*nums2[-1])
+        smallest = low
+
+        nums1_negs = [el for el in nums1 if el < 0]
+        nums1_zeroes = [el for el in nums1 if el == 0]
+        nums1_poss = [el for el in nums1 if el > 0]
+        nums2_negs = [el for el in nums2 if el < 0]
+        nums2_zeroes = [el for el in nums2 if el == 0]
+        nums2_poss = [el for el in nums2 if el > 0]
+
+        def count_leq(x):
+            count = 0
+            
+            if nums1_zeroes and x >= 0:
+                count += len(nums1_zeroes) * len(nums2)
+            elif nums2_zeroes and x >= 0:
+                count += len(nums1_zeroes) * len(nums2_zeroes)
+            
+
+
+        def count_leq(x):
+            count = 0
+            incrementor = 0
+            for idx, el in enumerate(nums1):
+                if el > 0:
+                    while len(nums2) - incrementor > 0:
+                        if el * nums2[len(nums2) - incrementor - 1] > x:
+                            incrementor += 1
+                            if incrementor == len(nums2):
+                                incrementor = 0
+                                break
+                        else:
+                            count += len(nums2) - incrementor
+                            if (idx < len(nums1) - 1) and ((nums1[idx] < 0 and nums1[idx + 1] >= 0) or (nums1[idx] >= 0 and nums1[idx+1] < 0) or (nums1[0] < 0 and nums1[-1] <0) or (nums2[0] < 0 and nums2[-1] <0)):
+                                incrementor = 0
+                            break
+                else:
+                    while incrementor < len(nums2):
+                        if el * nums2[incrementor] > x:
+                            incrementor += 1
+                            if incrementor == len(nums2):
+                                incrementor = 0
+                                break
+                        else:
+                            count += len(nums2) - incrementor
+                            if (idx < len(nums1) - 1) and ((nums1[idx] < 0 and nums1[idx + 1] >= 0) or (nums1[idx] >= 0 and nums1[idx+1] < 0) or (nums1[0] < 0 and nums1[-1] <0) or (nums2[0] < 0 and nums2[-1] <0)):
+                                incrementor = 0
+                            break
+            return count
+        
         while low < high:
             mid = low + (high - low) // 2
-            if nums1[cursor1] * nums2[cursor2] <= mid:
-                counter += 1
-                cursor2 += cursor2_incrementor
-            elif cursor2 < len(nums2) - 1:
-                cursor2 += cursor2_incrementor
-            elif cursor1 < len(nums1) - 1:
-                cursor1 += cursor1_incrementor
-                cursor2 = cursor2_start
+            if count_leq(mid) >= k:
+                high = mid
+                smallest = high
             else:
-                if counter >= k:
-                    high = mid - 1
-                    counter = 0
-                else:
-                    low = mid + 1
-                cursor1 = cursor1_start
-                cursor2 = cursor2_start
-        return mid
+                low = mid + 1
+                smallest = low
+        return smallest
+
+
 
 s = Solution()
-print(s.kthSmallestProduct([4, 5, 6, 7], [100, 1000, 10000], 10))
-
-# r & l neg
-[-2 -1], [-3, -2]
-
-# r & l neg-to-pos
-[-1, 0], [-1, 0]
-
-# l neg, r neg-to-pos
-[-2 -1], [-1, 3]
-
-# l neg-to-pos, r neg
-[-1, 1], [-2, -1]
-
-# l pos, r neg-to-pos
-[0, 1], [-1, 0]
-
-# l neg, r pos
-[-2, -1], [1, 2]
-
-# l pos, r neg
-[1, 2], [-2, -1]
-
-# r & l pos
-[0, 1], [0, 1]
+print(s.kthSmallestProduct([-3,-1,5,6], [-10,-7,-6,-5,-5,-4,-1,7,8], 28))
