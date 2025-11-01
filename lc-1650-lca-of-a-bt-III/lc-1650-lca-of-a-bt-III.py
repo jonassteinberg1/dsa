463ad34a0e5e53ac4c735ef12e47dde6654c767e
jsteinberg-rbi <jsteinberg@rbi.com>
2025-11-01 13:25:12 -0400

    iterative solution is easier

--- lc-1650-lca-of-a-bt-III/lc-1650-lca-of-a-bt-III.py
+++ lc-1650-lca-of-a-bt-III/lc-1650-lca-of-a-bt-III.py
@@ -7,22 +7,39 @@ class Node:
 
 class Solution:
     def lowestCommonAncestor(self, p: Node, q: Node) -> Node:
-        if p is q:
+        p_start = p
+        q_start = q
+        if not p.parent:
             return p
-        self.lowestCommonAncestor(p.parent, q.parent)
-        
+        if not q.parent:
+            return q
+        while p and q:
+            if p is q:
+                return p
+            elif p.parent and q.parent:
+                p = p.parent
+                q = q.parent
+            elif not p.parent:
+                p = q_start
+            else:
+                q = p_start
+
 
 n1 = Node(1)
 n2 = Node(2)
 n3 = Node(3)
 n4 = Node(4)
-
-
 n1.left = n2
+n1.right = n3
+n2.left = n4
 n2.parent = n1
-n2.left = n3
-n3.parent = n2
-n2.right = n4
+n3.parent = n1
 n4.parent = n2
 
-Solution().lowestCommonAncestor(n3, n4)
+#n1 = Node(1)
+#n2 = Node(2)
+#n1.left = n2
+#n2.parent = n1
+
+
+Solution().lowestCommonAncestor(n4, n1)
