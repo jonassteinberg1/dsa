b199e364984dc2102bfaf1cd9975978a93c6866c
jsteinberg-rbi <jsteinberg@rbi.com>
2025-11-17 18:27:08 -0500

    98 init

---
+++ lc-98-validate-bst/lc-98-validate-bst.py
@@ -0,0 +1,11 @@
+from typing import Optional
+
+# Definition for a binary tree node.
+class Node:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[Node]) -> bool:
+        pass
