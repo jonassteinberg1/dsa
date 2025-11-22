99cb1591e6e36b63b99dd6946714286523bd50fa
jsteinberg-rbi <jsteinberg@rbi.com>
2025-11-22 14:14:11 -0500

    straighten out endpoint discount, should be good after that

--- lc-2603-coins-in-a-tree/lc-2603-coins-in-a-tree.py
+++ lc-2603-coins-in-a-tree/lc-2603-coins-in-a-tree.py
@@ -1,5 +1,64 @@
+from collections import defaultdict, deque
 from typing import List
 
 class Solution:
     def collectTheCoins(self, coins: List[int], edges: List[List[int]]) -> int:
-        pass
+        if sum(coins) == 0:
+            return 0
+        
+        adj = defaultdict(list)
+        # edges compacts u, v and v, u into [u, v]
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        
+        # build degree list from adjacency list
+        degrees = {u: len(v) for u, v in adj.items()}
+
+        d = deque()
+
+        # enqueue with coinless leaves first
+        for u in adj.keys():
+            if degrees[u] == 1 and coins[u] == 0:
+                d.append(u)
+        
+        # BFS prune 1: coinless leaves
+        while d:
+            u = d.popleft()
+            # no longer in the graph; deleting would be cleaner
+            degrees[u] = 0
+            # can also skip "v" and just use the expression
+            # adj[u][0] everywhere but u and v are "node" and "neighbor"
+            # conventions
+            v = adj[u][0]
+            degrees[v] -= 1
+            if degrees[v] == 1 and coins[v] == 0:
+                d.append(v)
+        
+        for u in adj.keys():
+            if degrees[u] == 1:
+                # d[u][1] is depth and tracking depth
+                # is required for endpoint discount
+                d.append((u, 0))
+        
+        while d:
+            u = d.popleft()
+            depth = u[1]
+            if depth < 2:
+                degrees[u] = 0
+            for v in adj[u]:
+                degrees[v] -= 1
+                if degrees[v] == 1:
+                    d.append((v, depth + 1))
+            
+
+
+                
+
+
+
+
+        
+
+s = Solution()
+s.collectTheCoins([1,0,0,0,0,1], [[0,1],[1,2],[2,3],[3,4],[4,5]])
