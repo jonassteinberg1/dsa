f76aac5168657fe5b64849a44b5c75dd7974bcb9
jsteinberg-rbi <jsteinberg@rbi.com>
2025-12-04 19:11:18 -0500

    working on storing step state

--- lc-864-shortest-path-to-get-all-keys/lc-864-shortest-path-to-get-all-keys.py
+++ lc-864-shortest-path-to-get-all-keys/lc-864-shortest-path-to-get-all-keys.py
@@ -13,7 +13,14 @@ class Solution:
                     if m == '@':
                         raise Found
         except Found:
-            start = [i, j, set()]
+            start = [i, j, set(), 0]
+        
+        keys = []
+
+        for i, l in enumerate(grid):
+            for j in l:
+                if j in ['a', 'b', 'c', 'd', 'e', 'f', 'g']:
+                    keys.append(j)
 
         d = deque()
         d.append(start)
@@ -23,19 +30,20 @@ class Solution:
         def look(node: List[int]) -> List[str]:
                 i = node[0]
                 j = node[1]
+                key_state = node[2]
                 move = []
                 # index condition for looking up
-                if i > 0 and [i-1, j] not in seen:
-                    move.append([i-1, j], set())
+                if i > 0 and [i-1, j, key_state] not in seen:
+                    move.append([i-1, j, key_state])
                 # index condition for looking down
-                if i < len(grid) - 1 and [i+1, j] not in seen:
-                    move.append([i+1, j], set())
+                if i < len(grid) - 1 and [i+1, j, key_state] not in seen:
+                    move.append([i+1, j, key_state])
                 # index condition for looking left
-                if j > 0 and [i, j-1] not in seen:
-                    move.append([i, j-1], set())
+                if j > 0 and [i, j-1, key_state] not in seen:
+                    move.append([i, j-1, key_state])
                 # index condition for looking right
-                if j < len(l) - 1 and [i, j+1] not in seen:
-                    move.append([i, j+1], set())
+                if j < len(l) - 1 and [i, j+1, key_state] not in seen:
+                    move.append([i, j+1, key_state])
         
                 return move
 
@@ -44,19 +52,23 @@ class Solution:
             current = d.popleft() # need to mark visited next
             moves = look(current)
             for move in moves:
-                seen.append(move)
+                seen.append(move[0:3])
                 if grid[move[0]][move[1]] == '#':
                     pass
                 elif grid[move[0]][move[1]] == '.':
+                    move[3] += 1
                     d.append(move)
-                elif grid[move[0]][move[1]] in ['a', 'b', 'c', 'd', 'e', 'f']:
+                elif grid[move[0]][move[1]] in keys:
                     move[2].add(grid[move[0]][move[1]])
+                    if move[2] == keys:
+                        move[3] += 1
+                        return move[3]
+                    move[3] += 1
                     d.append(move)
                 else:
                     if grid[move[0]][move[1]].lower() in move[2]:
+                        move[3] += 1
                         d.append(move)
-
-        return 
 
 s = Solution()
 s.shortestPathAllKeys(["@..aA","..B#.","....b"])
