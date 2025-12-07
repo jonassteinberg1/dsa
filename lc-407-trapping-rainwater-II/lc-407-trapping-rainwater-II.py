092bccc9091b6190f675e567db344fcb2044c548
jsteinberg-rbi <jsteinberg@rbi.com>
2025-12-07 14:17:34 -0500

    init

---
+++ lc-407-trapping-rainwater-II/lc-407-trapping-rainwater-II.py
@@ -0,0 +1,24 @@
+from typing import List
+
+import heapq
+
+class Solution:
+    def trapRainWater(self, heightMap: List[List[int]]) -> int:
+
+        # need at least 3 grid rows
+        # to trap water
+        if len(heightMap) < 3:
+            return 0
+
+        water = []
+
+        for h in heightMap:
+            for i, j in enumerate(h):
+                # up
+                if i > 0:
+                    h[i - 1] - h[i]
+                # left
+                if 
+
+s = Solution()
+s.trapRainWater([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
