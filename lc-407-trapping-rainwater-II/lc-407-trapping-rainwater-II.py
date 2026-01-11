2cd297578a42ba7be23a80cbbd1367cf3a3e79db
jsteinberg-rbi <jsteinberg@rbi.com>
2026-01-11 16:29:48 -0500

    begin look bfs algo

--- lc-407-trapping-rainwater-II/lc-407-trapping-rainwater-II.py
+++ lc-407-trapping-rainwater-II/lc-407-trapping-rainwater-II.py
@@ -11,17 +11,17 @@ class Solution:
             return 0
 
         water = []
+        neighbors = []
 
         for h in heightMap:
             for i, j in enumerate(h):
-                # left: consider dropping lists that can't
-                # be trap candidates for example the edge lists
-                # and also drop the edge neighbors
-                # up
-                if i > 0:
-                    h[i - 1] - h[i]
-                # left
-                if 
+                # 1. a non-edge height differences imply trapability
+                # 2. non-edge height differences are okay
+                # look:
+                #   if you're an edge -- no
+                #   if you're not an edge -- maybe
+                #     if neighbor_diff > 0
+                #       trapability == least_neighbor_diff
 
 s = Solution()
 s.trapRainWater([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
