ea8fcd83bc2e1dc38b6a773e72f157a4beae0b6d
jsteinberg-rbi <jsteinberg@rbi.com>
2026-02-04 07:14:55 -0500

    7/42 test cases passing

--- lc-407-trapping-rainwater-II/lc-407-trapping-rainwater-II.py
+++ lc-407-trapping-rainwater-II/lc-407-trapping-rainwater-II.py
@@ -11,17 +11,52 @@ class Solution:
             return 0
 
         water = []
-        neighbors = []
+        neighbors = [(1, 1)]
+        visited = []
 
-        for h in heightMap:
-            for i, j in enumerate(h):
-                # 1. a non-edge height differences imply trapability
-                # 2. non-edge height differences are okay
-                # look:
-                #   if you're an edge -- no
-                #   if you're not an edge -- maybe
-                #     if neighbor_diff > 0
-                #       trapability == least_neighbor_diff
+        # left off: if you have a difference in all four directions
+        # the min of that difference is the water (?)
+        # you could say if len(height_differences) == 4 then max?
+        
+
+        def look(start: List[int, int]):
+            height_differences = []
+            up = (start[0], start[1] - 1)
+            up_height_difference = heightMap[start[0]][start[1]] - heightMap[up[0]][up[1]]
+            if up_height_difference < 0 and up not in visited:
+                heapq.heappush(height_differences, up_height_difference)
+            if up[0] != 0 and up[0] != len(heightMap) - 1 and up[1] != 0 and up[1] != len(heightMap[up[0]]) - 1 and up not in visited and up not in neighbors:
+                neighbors.append(up)
+            left = (start[0] + 1, start[1])
+            left_height_difference = heightMap[start[0]][start[1]] - heightMap[left[0]][left[1]]
+            if left_height_difference < 0 and left not in visited:
+                heapq.heappush(height_differences, left_height_difference)
+            if left[0] != 0 and left[0] != len(heightMap) - 1 and left[1] != 0 and left[1] != len(heightMap[up[0]]) - 1 and left not in visited and left not in neighbors:
+                neighbors.append(left)
+            down = (start[0], start[1] + 1)
+            down_height_difference = heightMap[start[0]][start[1]] - heightMap[down[0]][down[1]]
+            if down_height_difference < 0 and down not in visited:
+                heapq.heappush(height_differences, down_height_difference)
+            if down[0] != 0 and down[0] != len(heightMap) - 1 and down[1] != 0 and down[1] != len(heightMap[up[0]]) - 1 and down not in visited and down not in neighbors:
+                neighbors.append(down)
+            right = (start[0] - 1, start[1])
+            right_height_difference = heightMap[start[0]][start[1]] - heightMap[right[0]][right[1]]
+            if right_height_difference < 0 and right not in visited:
+                heapq.heappush(height_differences, right_height_difference)
+            if right[0] != 0 and right[0] != len(heightMap) - 1 and right[1] != 0 and right[1] != len(heightMap[up[0]]) - 1 and right not in visited and right not in neighbors:
+                neighbors.append(right)
+            
+            return height_differences
+        
+        while neighbors:
+            current = heapq.heappop(neighbors)
+            visited.append(current)
+            height_differences = look(current)
+            if height_differences:
+                water.append(height_differences.pop())
+        
+        return -1 * sum(water)
+
 
 s = Solution()
-s.trapRainWater([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
+s.trapRainWater([[3,3,3,],[3,2,3],[3,1,3],[3,2,3],[3,3,3]])
